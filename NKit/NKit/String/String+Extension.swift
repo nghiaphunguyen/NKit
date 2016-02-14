8d08d32af739485ae98707798aa65a3ab96e0aeb
Nghia Nguyen <nghia@misfit.com>
2016-02-14 14:53:00 +0700

    update subscript

--- NKit/NKit/String/String+Extension.swift
+++ NKit/NKit/String/String+Extension.swift
@@ -14,12 +14,10 @@ public func ++(left: String, right: String) -> String {
 }
 
 public extension String {
-    subscript (r: Range<Int>) -> String {
-        get {
-            let startIndex = advance(self.startIndex, r.startIndex)
-            let endIndex = advance(startIndex, r.endIndex - r.startIndex)
-            
-            return self[Range(start: startIndex, end: endIndex)]
-        }
+    public subscript(r: Range<Int>) -> String {
+        let startIndex = self.startIndex.advancedBy(r.startIndex)
+        let endIndex = self.startIndex.advancedBy(min(self.characters.count, r.endIndex))
+        
+        return self[Range(start: startIndex, end: endIndex)]
     }
 }
