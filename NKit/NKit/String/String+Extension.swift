0007756d467d8d641e5c4c8fb0dece636092cbf9
Nghia Nguyen <nghia@misfit.com>
2016-02-14 14:28:31 +0700

    add string subscript to get range

--- NKit/NKit/String/String+Extension.swift
+++ NKit/NKit/String/String+Extension.swift
@@ -12,3 +12,14 @@ infix operator ++ {}
 public func ++(left: String, right: String) -> String {
     return (left as NSString).stringByAppendingPathComponent(right)
 }
+
+public extension String {
+    subscript (r: Range<Int>) -> String {
+        get {
+            let startIndex = advance(self.startIndex, r.startIndex)
+            let endIndex = advance(startIndex, r.endIndex - r.startIndex)
+            
+            return self[Range(start: startIndex, end: endIndex)]
+        }
+    }
+}
