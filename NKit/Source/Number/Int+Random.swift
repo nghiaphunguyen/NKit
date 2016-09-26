dfd5ebe1d1df772ce4ad18fee31dfc9ee77b6717
Nghia Nguyen <nghianp@fastsell.vn>
2016-09-26 14:49:17 +0700

    KillOptional

---
+++ NKit/Source/Number/Int+Random.swift
@@ -0,0 +1,30 @@
+//
+//  Double+Random.swift
+//  NKit
+//
+//  Created by Nghia Nguyen on 9/26/16.
+//  Copyright © 2016 Nghia Nguyen. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+    private struct Dummy {
+        static var Template: String {return "This is a protoype string"}
+    }
+    
+    public var nk_dummyString: String {
+        let templateCount = Dummy.Template.characters.count
+        let times = UInt((self / templateCount) + 1)
+        return (Dummy.Template * times)[0..<self]
+    }
+    
+    public var nk_dummyNumber: String {
+        var result = ""
+        for _ in 0..<self {
+            result += "\(rand() % 10)"
+        }
+        
+        return result
+    }
+}
---
+++ NKit/Source/Utilities/KillOptional.swift
@@ -0,0 +1,83 @@
+//
+//  KillOptional.swift
+//  NKit
+//
+//  Created by Nghia Nguyen on 9/26/16.
+//  Copyright © 2016 Nghia Nguyen. All rights reserved.
+//
+
+import Foundation
+
+infix operator ?! {}
+public func ?! <T>(left: T?, right: (T) -> Void){
+    if let left = left {
+        right(left)
+    }
+}
+
+public func ?! <T>(left: T?, right: () -> Void){
+    if let _ = left {
+        right()
+    }
+}
+
+public func ?! <T, U>(left: (T?, U?), right: (T, U) -> Void) {
+    if let t = left.0, u = left.1 {
+        right(t, u)
+    }
+}
+
+public func ?! <T, U>(left: (T?, U?), right: () -> Void) {
+    if let _ = left.0, _ = left.1 {
+        right()
+    }
+}
+
+public func ?! <T, U, V>(left: (T?, U?, V?), right: (T, U, V) -> Void) {
+    if let t = left.0, u = left.1, v = left.2 {
+        right(t, u, v)
+    }
+}
+
+public func ?! <T, U, V>(left: (T?, U?, V?), right: () -> Void) {
+    if let _ = left.0, _ = left.1, _ = left.2 {
+        right()
+    }
+}
+
+public func ?! <T, U, V, X>(left: (T?, U?, V?, X?), right: (T, U, V, X) -> Void) {
+    if let t = left.0, u = left.1, v = left.2, x = left.3 {
+        right(t, u, v, x)
+    }
+}
+
+public func ?! <T, U, V, X>(left: (T?, U?, V?, X?), right: () -> Void) {
+    if let _ = left.0, _ = left.1, _ = left.2, _ = left.3 {
+        right()
+    }
+}
+
+public func ?! <T, U, V, X, Y>(left: (T?, U?, V?, X?, Y?), right: (T, U, V, X, Y) -> Void) {
+    if let t = left.0, u = left.1, v = left.2, x = left.3, y = left.4 {
+        right(t, u, v, x, y)
+    }
+}
+
+public func ?! <T, U, V, X, Y>(left: (T?, U?, V?, X?, Y?
+    ), right: () -> Void) {
+    if let _ = left.0, _ = left.1, _ = left.2, _ = left.3, _ = left.4 {
+        right()
+    }
+}
+
+public func ?! <T, U, V, X, Y, W>(left: (T?, U?, V?, X?, Y?, W?), right: (T, U, V, X, Y, W) -> Void) {
+    if let t = left.0, u = left.1, v = left.2, x = left.3, y = left.4, w = left.5 {
+        right(t, u, v, x, y, w)
+    }
+}
+
+public func ?! <T, U, V, X, Y, W>(left: (T?, U?, V?, X?, Y?, W?), right: () -> Void) {
+    if let _ = left.0, _ = left.1, _ = left.2, _ = left.3, _ = left.4, _ = left.5 {
+        right()
+    }
+}
