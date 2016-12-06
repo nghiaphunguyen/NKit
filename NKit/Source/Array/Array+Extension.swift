19f563d32eec34e7f3a7321f5f8f00015dacd67f
Nghia Nguyen <nghianp@fastsell.vn>
2016-12-06 21:00:27 +0700

    cherry pick .0.9.22

---
+++ NKit/Source/Array/Array+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  Array+Extension.swift
+//  NKit
+//
+//  Created by Nghia Nguyen on 12/6/16.
+//  Copyright © 2016 Nghia Nguyen. All rights reserved.
+//
+
+import Foundation
+import NRxSwift
+
+public extension Array where Element: NKOptional {
+    public var nk_unwrap: Array<Element.Wrapped> {
+        return self.filter {$0.value != nil}.map {$0.value!}
+    }
+}
+
+public extension Array {
+    public var nk_any: [Any] {
+        return self.map {$0 as Any}
+    }
+}
+
+public extension Array where Element: Any {
+    public func nk_cast<T>() -> [T] {
+        return self.map {$0 as? T}.nk_unwrap
+    }
+}
