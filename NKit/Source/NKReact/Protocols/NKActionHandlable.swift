4e4bb929191ca18443509770aa3e2427e6e1a71c
nghianguyen <nghia.nguyen@thecarousell.com>
2017-03-25 21:22:59 +0700

    Bump to 2.1.2

--- NKit/Source/NKReact/Protocols/NKActionHandlable.swift
+++ NKit/Source/NKReact/Protocols/NKActionHandlable.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 
-public protocol ActionHandlable {
+public protocol NKActionHandlable {
     var actionHub: NKActionDelegable {get}
     var sender: AnyObject? {get}
     func setupActionHandler()
     func handle(action: NKAction)
 }
 
-extension ActionHandlable where Self: NSObject {
-    func setupActionHandler() {
+extension NKActionHandlable where Self: NSObject {
+    public func setupActionHandler() {
         self.actionHub.handleActionObservable.filter {[weak self] in $0.isEqualSender(self?.sender) == true
             }.nk_subscribe { (action) in
             self.handle(action: action)
