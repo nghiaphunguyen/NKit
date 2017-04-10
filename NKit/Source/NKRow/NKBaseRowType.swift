6adeddb2a5ef76fd7deb16584645f073bfcd7f49
nghianguyen <nghia.nguyen@thecarousell.com>
2017-04-10 11:41:25 +0700

    Bump to 2.1.40

--- NKit/Source/NKRow/NKBaseRowType.swift
+++ NKit/Source/NKRow/NKBaseRowType.swift
@@ -18,18 +18,10 @@ public extension NKBaseRowType where Self: UICollectionViewCell {
     public var sender: AnyObject? {
         return self.nk_collectionView
     }
-    
-    public var indexPath: IndexPath? {
-        return self.nk_collectionView?.indexPath(for: self)
-    }
 }
 
 public extension NKBaseRowType where Self: UITableViewCell {
     public var sender: AnyObject? {
         return self.nk_tableView
-    }
-    
-    public var indexPath: IndexPath? {
-        return self.nk_tableView?.indexPath(for: self)
     }
 }
