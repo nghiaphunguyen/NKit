1837e070998f7cb0903850b69151eb901bf707ec
stephen <stephennguyen@linecorp.com>
2021-09-23 22:44:02 +0700

    add objc key

--- NKit/Source/UIViewController/UIViewController+ChildViewController.swift
+++ NKit/Source/UIViewController/UIViewController+ChildViewController.swift
@@ -7,43 +7,13 @@
 //
 
 import UIKit
-
-protocol Check {
-    associatedtype State
-    associatedtype Action
-    
-    var state: State {get}
-    var action: Action {get}
-}
-
-protocol CheckState {
-    var a: Int {get}
-    
-}
-
-protocol CheckAction {
-    
-}
-
-
-class CheckReactor<A, B> {
-    typealias State = A
-    typealias Action = B
-
-    var state: State!
-    var action: Action!
-}
-
-let b = CheckReactor<CheckState, CheckAction>()
-
-
 public extension UIViewController {
-    public func nk_addChildViewController(controller: UIViewController) {
+    @objc public func nk_addChildViewController(controller: UIViewController) {
         self.addChild(controller)
         controller.didMove(toParent: self)
     }
     
-    public func nk_removeFromParentViewController() {
+    @objc public func nk_removeFromParentViewController() {
         self.willMove(toParent: nil)
         self.view.removeFromSuperview()
         self.removeFromParent()
@@ -52,7 +22,7 @@ public extension UIViewController {
 
 
 public extension UIViewController {
-    public func nk_hideKeyboardWhenTappedOuside() {
+    @objc public func nk_hideKeyboardWhenTappedOuside() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.nk_hideKeyboard))
         self.view.addGestureRecognizer(tapGesture)
     }
